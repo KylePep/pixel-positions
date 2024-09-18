1b9cdc16fb85e5e0829403eefff0651cca893768
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 10:58:26 -0600

    grouping jobs in controller get request, tag factory fix, database seeder added JobSeeder call, created JobSeeder, Front end dynamically supports data.

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['size' => 'base'])
+@props(['tag', 'size' => 'base'])
 
 @php
 $classes = "bg-white/10 hover:bg-white/25 rounded-xl transition-colors duration-300";
@@ -10,6 +10,6 @@ $classes .= " px-3 py-1 text-2xs";
 }
 @endphp
 
-<a class="{{ $classes}}" href="">
-  {{$slot}}
+<a class="{{ $classes}}" href="/tags/{{strtolower($tag->name)}}">
+  {{ $tag->name }}
 </a>
