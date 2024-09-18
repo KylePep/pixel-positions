0d9ac0393a5098a9bfa3bab981643d01c1d77e90
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 10:17:46 -0600

    used TDD to create a test that assisted in the creation of tags for jobs

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -14,3 +14,12 @@ it('belongs to an employer', function(){
     expect($job->employer->is($employer))->toBeTrue();
 
 });
+
+it('can have tags', function(){
+    //AAA
+    $job = Job::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
+});
