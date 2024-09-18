80d9e5fc10726a75ebaf0043da26949f5001c9a4
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 08:16:43 -0600

    components css and layout

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 90])
+
+<img src="https://picsum.photos/id/{{ rand(0, 100)}}/{{$width}}" class="rounded-xl bg-white/25" alt="">
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,3 @@
-<a class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300"
-  href="">{{$slot}}</a>
+<a class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-2xs transition-colors duration-300" href="">
+  {{$slot}}
+</a>
