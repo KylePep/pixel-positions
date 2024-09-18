9fd55945c58cab2acf1d6a2ef9f1db09d7ace536
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 08:35:40 -0600

    tag->configurable

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -9,9 +9,9 @@
 
   <div class="flex justify-between items-center mt-auto">
     <div>
-      <x-tag>Tag</x-tag>
-      <x-tag>Tag</x-tag>
-      <x-tag>Tag</x-tag>
+      <x-tag size="small">Backend</x-tag>
+      <x-tag size="small">Frontend</x-tag>
+      <x-tag size="small">Manager</x-tag>
 
     </div>
 
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,15 @@
-<a class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-2xs transition-colors duration-300" href="">
+@props(['size' => 'base'])
+
+@php
+$classes = "bg-white/10 hover:bg-white/25 rounded-xl transition-colors duration-300";
+if ($size ==='base'){
+$classes .= " px-5 py-1 text-2sm";
+}
+if ($size ==='small'){
+$classes .= " px-3 py-1 text-2xs";
+}
+@endphp
+
+<a class="{{ $classes}}" href="">
   {{$slot}}
 </a>
