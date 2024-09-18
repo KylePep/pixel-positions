ecefa7af11679ab15e023d347bc3e5956fae6fc2
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 10:05:08 -0600

    testing, creating job assets, fixed job, employer model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Employer extends Model
+class Job extends Model
 {
     use HasFactory;
-
-    public function user(): BelongsTo
+    
+    public function employer()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(Employer::class);
     }
 }
